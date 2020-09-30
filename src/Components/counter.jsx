3a08047fb58769b9516279dd7dc6ed9a60a18b1a
jatan-ops <jatan.shah90@gmail.com>
2020-09-30 23:47:06 +0530

    changed button text

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -17,7 +17,7 @@ class Counter extends Component {
           onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
-          IncrementButton
+          Increment
         </button>
       </div>
     );
