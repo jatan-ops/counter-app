de6b65d92e735896425695e9e37da74bfc489023
jatan-ops <jatan.shah90@gmail.com>
2020-09-30 23:33:39 +0530

    upload from PC

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Counters from "./Components/counters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counters />
+    {/* wherever we have Counter element, we'll get output of Counter component. This is det'd by render method in Counter component */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
